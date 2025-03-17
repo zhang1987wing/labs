cffb5366250dfd227ecab2fac58bc211519d5646
zhang1987wing <thisiszy@gmail.com>
2025-03-17 20:01:33 +0800

    update

--- stock.py
+++ stock.py
@@ -9,7 +9,8 @@ def get_stock_data(stock_code, start_date, end_date):
     stock_data = ak.stock_zh_a_hist(symbol=stock_code, period="daily", start_date=start_date, end_date=end_date,
                                     adjust="qfq")
     print(stock_data.columns.tolist())
-    stock_data.columns = ['日期', '股票代码', '开盘', '收盘', '最高', '最低', '成交量', '成交额', '振幅', '涨跌幅', '涨跌额', '换手率']
+    stock_data.columns = ['日期', '股票代码', '开盘', '收盘', '最高', '最低', '成交量', '成交额', '振幅', '涨跌幅',
+                          '涨跌额', '换手率']
 
     # 将日期转换为索引
     stock_data['日期'] = pd.to_datetime(stock_data['日期'])
@@ -44,6 +45,9 @@ def calculate_indicators(stock_data):
     stock_data["dmi_plus"] = talib.PLUS_DI(high, low, close, timeperiod=14)
     stock_data["dmi_minus"] = talib.MINUS_DI(high, low, close, timeperiod=14)
 
+    # atr指标
+    stock_data["atr"] = talib.ATR(high, low, close, timeperiod=14)
+
 
 # 获取板块行情数据
 def get_board_concept_name_df():
@@ -68,17 +72,36 @@ def get_board_concept_name_df():
     print(board_df.tail(10))
 
 
+def sell_price_strategy(high, low, atr):
+    height = high - low
+
+    if height <= atr:
+        sell_price = low - atr
+    elif atr < height < 2 * atr:
+        sell_price = low - 1.5 * atr
+    else:
+        sell_price = low - 2 * atr
+
+    return sell_price
+
+
 def trade_strategy(stock_data):
     buy_date = ''
     position = 0  # 持仓状态 (0: 空仓, 1: 持仓)
     buy_price = 0
     capital = 100000  # 初始资金
     holdings = 0
+    buy_date_idx = 0
+    atr_sell_price = 0.0
+    cooldown_days = 5
+    last_sell_idx = 0
 
     trade_log = []
 
     for i in range(1, len(stock_data)):
         close = stock_data["收盘"].iloc[i]
+        highest = stock_data["最高"].iloc[i]
+        lowest = stock_data["最低"].iloc[i]
         lower = stock_data["BBANDS_lower"].iloc[i]
         upper = stock_data["BBANDS_upper"].iloc[i]
         ma5 = stock_data["ma5"].iloc[i]
@@ -88,11 +111,19 @@ def trade_strategy(stock_data):
         dea = stock_data['macd_dea'].iloc[i]
         dmi_plus = stock_data['dmi_plus'].iloc[i]
         dmi_minus = stock_data['dmi_minus'].iloc[i]
+        atr = stock_data['atr'].iloc[i]
 
         macd_strategy = macd > 0 and dif > dea
         dmi_strategy = dmi_plus > dmi_minus
-        bbands_strategy = close < lower
+        bbands_strategy = True
         ma_strategy = ma5 > ma10
+
+        # 检查是否在冷却期内
+        if last_sell_idx != 0:
+            days_since_sell = i - last_sell_idx
+            if days_since_sell <= cooldown_days:
+                # 在冷却期内，不允许买入或卖出
+                continue
 
         # 买入条件
         if position == 0 and macd_strategy and dmi_strategy and bbands_strategy and ma_strategy:
@@ -101,18 +132,28 @@ def trade_strategy(stock_data):
             holdings = capital // buy_price
             capital -= holdings * buy_price
             buy_date = stock_data.index[i]
+            buy_date_idx = i
             trade_log.append(f"BUY: {stock_data.index[i]} at {buy_price}")
+            atr_sell_price = sell_price_strategy(highest, lowest, atr)
 
         # 卖出条件
         elif position == 1:
-            days_held = (stock_data.index[i] - buy_date).days
+            date = stock_data.index[i].date()
+            days_held = i - buy_date_idx
             profit_ratio = (close - buy_price) / buy_price
+            profit_strategy = profit_ratio > 0.10
+            days_held_strategy = days_held > 10
+            atr_strategy = close < atr_sell_price
 
-            if macd_strategy == False or dmi_strategy == False or bbands_strategy == False or ma_strategy == False:
-                sell_price = close
+            sell_strategy = ((macd_strategy is np.False_) or (dmi_strategy is np.False_) or (bbands_strategy is False)
+                             or (ma_strategy is np.False_) or profit_strategy or days_held_strategy or atr_strategy)
+
+            if sell_strategy:
+                sell_price = atr_sell_price if atr_strategy else close
                 position = 0
                 capital += holdings * sell_price
                 holdings = 0
+                last_sell_idx = i
                 trade_log.append(
                     f"SELL: {stock_data.index[i].date()} at {sell_price:.2f} | Profit: {profit_ratio * 100:.2f}% | "
                     f"Days Held: {days_held}"
@@ -120,7 +161,7 @@ def trade_strategy(stock_data):
 
     # 最终资金 + 持有股票市值
     if position == 1:
-        capital += holdings * stock_data["close"].iloc[-1]
+        capital += holdings * stock_data["收盘"].iloc[-1]
 
     # 打印交易记录
     for trade in trade_log:
@@ -129,7 +170,7 @@ def trade_strategy(stock_data):
     print(f"\n💰 Final Capital: {capital:.2f} CNY")
 
 
-data = get_stock_data('002879', '20250101', '20250314')
+data = get_stock_data('002879', '20220101', '20220717')
 calculate_indicators(data)
 trade_strategy(data)
 
