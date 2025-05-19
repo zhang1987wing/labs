4e49b23c6de16774041dff95741de07d4b114204
zhang1987wing <thisiszy@gmail.com>
2025-05-19 16:25:43 +0800

    update

--- stock/stock_watchlist.py
+++ stock/stock_watchlist.py
@@ -21,61 +21,32 @@ def expected_buy_price(open, close, high, low, highest_250):
     if high == highest_250:
         return round(high * 1.05, 2)
 
-
-def watchlist_strategy(stock_data):
-    buy_date = ''
+# 超卖策略
+def func_oversell_strategy(stock_data):
     position = 0  # 持仓状态 (0: 空仓, 1: 持仓)
-    buy_price = 0
-    holdings = 0
-    buy_date_idx = 0
-    atr_sell_price = 0.0
-    cooldown_days = 0
-    last_sell_idx = 0
-
     trade_logs = []
-    stock_code = stock_data["股票代码"].iloc[0]
-    trade_count = 0
-    profit_count = 0
-    macd_signal_days = 0
 
     for i in range(1, len(stock_data)):
+        # debug
         formatted_date = stock_data.index[i].date().strftime('%Y-%m-%d')
 
         if formatted_date == '2024-02-02':
             print("debug")
 
-        open = stock_data["开盘"].iloc[i]
+        stock_code = stock_data["股票代码"].iloc[0]
         close = stock_data["收盘"].iloc[i]
-        prev_close = stock_data["收盘"].iloc[i - 1]
-        high = stock_data["最高"].iloc[i]
-        highest_250 = stock_data["highest_250"].iloc[i]
-        low = stock_data["最低"].iloc[i]
-        volume = stock_data["成交量"].iloc[i]
-        days_increase = stock_data["涨跌幅"].iloc[i]
-        BBANDS_lower = stock_data["BBANDS_lower"].iloc[i]
-        BBANDS_upper = stock_data["BBANDS_upper"].iloc[i]
-        BBANDS_middle = stock_data["BBANDS_middle"].iloc[i]
-        ma5 = stock_data["ma5"].iloc[i]
-        ma10 = stock_data["ma10"].iloc[i]
-        ma20 = stock_data["ma20"].iloc[i]
-        ma60 = stock_data["ma60"].iloc[i]
         ma250 = stock_data["ma250"].iloc[i]
-        macd = stock_data['macd'].iloc[i]
-        dif = stock_data['macd_dif'].iloc[i]
-        dea = stock_data['macd_dea'].iloc[i]
-        dmi_plus = stock_data['dmi_plus'].iloc[i]
-        dmi_minus = stock_data['dmi_minus'].iloc[i]
-        atr = stock_data['atr'].iloc[i]
-        volume_ma5 = stock_data["volume_ma5"].iloc[i]
+        BBANDS_lower = stock_data["BBANDS_lower"].iloc[i]
         rsi = stock_data['rsi'].iloc[i]
 
+        # 超卖策略
         ma250_oversell_strategy = (ma250 - close) / ma250 > 0.3
         bbands_oversell_strategy = close < BBANDS_lower
         rsi_oversell_strategy = rsi < 25
 
-        over_sold_strategy = bool(rsi_oversell_strategy and bbands_oversell_strategy and ma250_oversell_strategy)
+        strategy = bool(rsi_oversell_strategy and bbands_oversell_strategy and ma250_oversell_strategy)
 
-        add_watchlist = over_sold_strategy
+        add_watchlist = strategy
 
         if add_watchlist:
             if position == 0:
@@ -90,6 +61,58 @@ def watchlist_strategy(stock_data):
         else:
             position = 0
             continue
+
+    return trade_logs
+
+
+# ma5金叉策略
+def func_ma5_golden_cross_strategy(stock_data):
+    position = 0  # 持仓状态 (0: 空仓, 1: 持仓)
+    trade_logs = []
+
+    for i in range(1, len(stock_data)):
+        # debug
+        formatted_date = stock_data.index[i].date().strftime('%Y-%m-%d')
+
+        if formatted_date == '2024-02-02':
+            print("debug")
+
+        stock_code = stock_data["股票代码"].iloc[0]
+
+        close = stock_data["收盘"].iloc[i]
+        ma5 = round(stock_data["ma5"].iloc[i], 2)
+        ma10 = round(stock_data["ma10"].iloc[i], 2)
+        ma60 = round(stock_data["ma60"].iloc[i], 2)
+
+        # ma5均线金叉策略
+        ma510_strategy = bool(ma5 > ma10 and stock_data["ma5"].iloc[i - 1] <= stock_data["ma10"].iloc[i - 1])
+        ma60_strategy = close > ma60
+
+        add_watchlist = ma510_strategy and ma60_strategy
+
+        if add_watchlist:
+            if position == 0:
+                buy_price = close
+                position = 1
+                buy_date = stock_data.index[i]
+                trade_logs.append(stock_watching(stock_code, buy_date.date(), 1, buy_price))
+            else:
+                position = 1
+                trade_log = trade_logs[len(trade_logs) - 1]
+                trade_log.watching_days = trade_log.watching_days + 1
+        else:
+            position = 0
+            continue
+
+    return trade_logs
+
+
+def get_watchlist(stock_data):
+
+    stock_code = stock_data["股票代码"].iloc[0]
+
+    # trade_logs = func_oversell_strategy(stock_data)
+    trade_logs = func_ma5_golden_cross_strategy(stock_data)
 
     # 打印交易记录
     for trade_log in trade_logs:
@@ -106,7 +129,7 @@ def process_stock(stock_code):
         data = stock_indicators.get_stock_data(stock_code, '20230101', '20250516')
 
         stock_indicators.calculate_indicators(data)
-        watchlist_stock = watchlist_strategy(data)
+        watchlist_stock = get_watchlist(data)
 
         return watchlist_stock
     except Exception as e:
@@ -122,15 +145,17 @@ if __name__ == "__main__":
     file_exists = os.path.exists(output_file)
 
     watching_map = {}
-
+    '''
     stock_profits = stock_indicators.get_stock_code()
     '''
-    stock_key = '002717'
+    stock_key = '002688'
     stock_profits = {
         stock_key: 0,
         # '002261': 0
     }
-    '''
+
+    strategy_type = 2
+
     with open(output_file, mode='a', newline='', encoding='utf-8') as f:
         writer = csv.writer(f)
 
