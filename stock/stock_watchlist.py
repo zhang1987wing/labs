4933ba3c33d0878b2d0cb35ea4e07fb6bd53c1d1
zhang1987wing <thisiszy@gmail.com>
2025-05-23 20:01:30 +0800

    update

--- stock/stock_watchlist.py
+++ stock/stock_watchlist.py
@@ -169,7 +169,7 @@ if __name__ == "__main__":
     '''
     stock_profits = stock_indicators.get_stock_code()
     '''
-    stock_key = '002352'
+    stock_key = '002261'
     stock_profits = {
         stock_key: 0,
         # '002261': 0
